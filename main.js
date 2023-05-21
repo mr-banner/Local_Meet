7a3bbf4e3898d57b874eee6decc7431aa80f5ad3
Kamal kant sahu <sahukamalkant28@gmail.com>
2023-05-21 21:09:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,153 @@
+let client = AgoraRTC.createClient({mode:'rtc', codec:"vp8"})
+
+let config = {
+    appid:'f0904f4220b74747a9e3e585579bb5bf',
+    token:'007eJxTYLh3eMrm8tOf4m5a6wZ94f70j/v+bLnLiRmLkpw3nTqy+y6TAkOagaWBSZqJkZFBkrkJECZaphqnmlqYmppbJiWZJqW5W2WlNAQyMlyrEGZmZIBAEJ+TwSc/OTHHNzW1hIEBALz5IuY=',
+    uid:null,
+    channel:'LocalMeet',
+}
+
+let localTracks = {
+    audioTrack:null,
+    videoTrack:null
+}
+let localTrackState = {
+    audioTrackMuted:false,
+    videoTrackMuted:false
+}
+
+let remoteTracks = {}
+
+
+document.getElementById('join-btn').addEventListener('click', async () => {
+    config.uid = document.getElementById('username').value
+    await joinStreams()
+    document.getElementById('join-wrapper').style.display = 'none'
+    document.getElementById('footer').style.display = 'flex'
+})
+
+document.getElementById('mic-btn').addEventListener('click', async () => {
+    if(!localTrackState.audioTrackMuted){
+        await localTracks.audioTrack.setMuted(true);
+        localTrackState.audioTrackMuted = true
+        document.getElementById('mic-btn').src = 'img/mic_off.png'
+    }else{
+        await localTracks.audioTrack.setMuted(false)
+        localTrackState.audioTrackMuted = false
+        document.getElementById('mic-btn').src = 'img/mic_on.png'
+
+    }
+
+})
+
+
+
+document.getElementById('camera-btn').addEventListener('click', async () => {
+    if(!localTrackState.videoTrackMuted){
+        await localTracks.videoTrack.setMuted(true);
+        localTrackState.videoTrackMuted = true
+        document.getElementById('camera-btn').src='./img/video_edited.jpg'
+        // document.getElementById('camera-btn').style.backgroundColor ='rgb(255, 80, 80, 0.7)'
+    }else{
+        await localTracks.videoTrack.setMuted(false)
+        localTrackState.videoTrackMuted = false
+        document.getElementById('camera-btn').src='./img/video_on.jpg'
+
+    }
+
+})
+
+
+
+document.getElementById('leave-btn').addEventListener('click', async () => {
+    for (trackName in localTracks){
+        let track = localTracks[trackName]
+        if(track){
+            track.stop()
+            track.close()
+            localTracks[trackName] = null
+        }
+    }
+    await client.leave()
+    document.getElementById('footer').style.display = 'none'
+    document.getElementById('user-streams').innerHTML = ''
+    document.getElementById('join-wrapper').style.display = 'block'
+
+})
+let joinStreams = async () => {
+    
+    client.on("user-published", handleUserJoined);
+    client.on("user-left", handleUserLeft);
+
+
+    client.enableAudioVolumeIndicator(); 
+    client.on("volume-indicator", function(evt){
+        for (let i = 0; evt.length > i; i++){
+            let speaker = evt[i].uid
+            let volume = evt[i].level
+            if(volume > 0){
+                document.getElementById(`volume-${speaker}`).src = './img/volume_on.jpg'
+            }else{
+                document.getElementById(`volume-${speaker}`).src = './img/volume_off.jpg'
+            }
+            
+        
+            
+        }
+    });
+    [config.uid, localTracks.audioTrack, localTracks.videoTrack] = await  Promise.all([
+        client.join(config.appid, config.channel, config.token ||null, config.uid ||null),
+        AgoraRTC.createMicrophoneAudioTrack(),
+        AgoraRTC.createCameraVideoTrack()
+
+    ])
+    let player = `<div class="video-containers" id="video-wrapper-${config.uid}">
+                        <p class="user-uid"><img class="volume-icon" id="volume-${config.uid}" src="./img/volume_on.jpg" /> ${config.uid}</p>
+                        <div class="video-player player" id="stream-${config.uid}"></div>
+                  </div>`
+
+    document.getElementById('user-streams').insertAdjacentHTML('beforeend', player);
+    localTracks.videoTrack.play(`stream-${config.uid}`)
+    await client.publish([localTracks.audioTrack, localTracks.videoTrack])
+
+}
+
+
+let handleUserJoined = async (user, mediaType) => {
+    console.log('Handle user joined')
+
+    remoteTracks[user.uid] = user
+    await client.subscribe(user, mediaType)
+   
+    
+    if (mediaType === 'video'){
+        let player = document.getElementById(`video-wrapper-${user.uid}`)
+        console.log('player:', player)
+        if (player != null){
+            player.remove()
+        }
+ 
+        player = `<div class="video-containers" id="video-wrapper-${user.uid}">
+                        <p class="user-uid"><img class="volume-icon" id="volume-${user.uid}" src="./assets/volume-on.svg" /> ${user.uid}</p>
+                        <div  class="video-player player" id="stream-${user.uid}"></div>
+                      </div>`
+        document.getElementById('user-streams').insertAdjacentHTML('beforeend', player);
+         user.videoTrack.play(`stream-${user.uid}`)
+
+        
+
+          
+    }
+    
+
+    if (mediaType === 'audio') {
+        user.audioTrack.play();
+      }
+}
+
+
+let handleUserLeft = (user) => {
+    console.log('Handle user left!')
+    delete remoteTracks[user.uid]
+    document.getElementById(`video-wrapper-${user.uid}`).remove()
+}
